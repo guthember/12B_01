ad572d35a93e569b1c2bf53576a4ccece76375a7
guthember <guthember@gmail.com>
2016-12-18 10:17:10 +0100

    Elso megoldas

---
+++ KorEgyszeru/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KorEgyszeru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 0;
+            //string tmp = string.Empty;
+            double ter;
+            double ker;
+            Console.Write("Kerjuk add meg a kor sugarat: ");
+
+            //tmp = Console.ReadLine();
+            // r = Convert.ToInt32(tmp);
+            r = Convert.ToInt32(Console.ReadLine());
+
+            ker = 2 * r * Math.PI;
+            ter = Math.Pow(r, 2) * Math.PI; // r * r
+
+            // Console.WriteLine("A kerulet: {0:N2}",ker);
+            // Console.WriteLine("A terulet: {0:N2}",ter);
+            // két tizedesre kerekítve lehet kiiratni
+            Console.WriteLine("A kerulet: {0}", ker);
+            Console.WriteLine("A terulet: {0}", ter);
+
+            Console.ReadKey();
+        }
+    }
+}
